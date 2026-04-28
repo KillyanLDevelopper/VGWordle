eb529d3dde36ba5c6c9d3f9909f983a88beffcf0
KillyanLDeveloper <killyandu08@gmail.com>
2026-04-28 17:54:37 +0200

    feat(game): add addLetter and deleteLetter functions

--- js/game.js
+++ js/game.js
@@ -22,3 +22,48 @@ export function initGame(options = {}) {
 		lettersState: {}
 	};
 }
+
+export function addLetter(state, letter) {
+    if (state.gameState !== GameState.PLAYING) {
+        return state;
+    }
+
+    if (typeof letter !== 'string') {
+        return state;
+    }
+
+    const normalizedLetter = letter.trim().normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toUpperCase();
+
+    if (normalizedLetter.length !== 1) {
+        return state;
+    }
+
+    if (!/^[A-Z]$/.test(normalizedLetter)) return state;
+
+
+    if (state.currentGuess.length >= state.wordLength) {
+        return state;
+    }
+
+    return {
+        ...state,
+        currentGuess: state.currentGuess + normalizedLetter
+    };
+}
+
+export function deleteLetter(state) {
+    if (state.gameState !== GameState.PLAYING) {
+        return state;
+    }
+
+    if (state.currentGuess.length === 0) {
+        return state;
+    }
+
+    return {
+        ...state,
+        currentGuess: state.currentGuess.slice(0, -1)
+    };
+}
